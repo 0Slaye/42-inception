b5aaed7949e98bc8f89c74766a58ae12b3c44568
Slaye <uduterrage@gmail.com>
2024-11-27 12:50:11 +0100

    Fix: volume

--- Makefile
+++ Makefile
@@ -1,28 +1,8 @@
-default: up
-
-re: clean up
-
-up:
+default:
+	@mkdir -p /home/uwywijas/data/mariadb /home/uwywijas/data/wordpress
 	@docker compose -f ./sources/docker-compose.yml up --build
 
-stop:
-	@docker compose -f ./sources/docker-compose.yml stop
-
-down:
-	@docker compose -f ./sources/docker-compose.yml down
-
-show:
-	@docker ps
-
-show-all:
-	@docker ps -a
-
-remove-volume:
-	@rm -rf sources/requirements/mariadb/volume/ sources/requirements/wordpress/volume/
-
-clean: remove-volume
-	@mkdir sources/requirements/mariadb/volume
-	@mkdir sources/requirements/wordpress/volume
-
-reset:
+fclean:
+	@docker compose -f ./sources/docker-compose.yml down -v
+	@rm -rf /home/uwywijas/data/mariadb/ /home/uwywijas/data/wordpress/
 	@docker system prune -af
--- sources/requirements/wordpress/imports/wp_configure.sh
+++ sources/requirements/wordpress/imports/wp_configure.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 echo "wp_configure.sh [10s]"
 sleep 10
-if [ ! -f "${WP_PATH}/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "Creating WordPress config..."
-	su -s /bin/bash www-data -c "wp-cli.phar config create --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost=mariadb:3306 --path=${WP_PATH}"
+	su -s /bin/bash www-data -c "wp-cli.phar config create --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost=mariadb:3306 --path=/var/www/html"
 else
 	echo "WordPress is already configured."
 fi
-su -s /bin/bash www-data -c "wp-cli.phar core is-installed --path=${WP_PATH}"
+su -s /bin/bash www-data -c "wp-cli.phar core is-installed --path=/var/www/html"
 if [ $? -ne 0 ]; then
 	echo "Installing WordPress..."
-	su -s /bin/bash www-data -c "wp-cli.phar core install --path=${WP_PATH} \
+	su -s /bin/bash www-data -c "wp-cli.phar core install --path=/var/www/html \
 		--url=${WP_URL} \
 		--title=${WP_TITLE} \
 		--admin_user=${WP_ADMIN_USER} \
 		--admin_password=${WP_ADMIN_PASS} \
 		--admin_email=${WP_ADMIN_EMAIL}"
+	echo "Creating second user..."
+	su -s /bin/bash www-data -c "wp-cli.phar --path=/var/www/html user create ${WP_USER} ${WP_USER_EMAIL} --role=subscriber --user_pass=${WP_USER_PASSWORD}"
 else
 	echo "WordPress is already installed."
-fi
-USER_EXISTS=$(su -s /bin/bash www-data -c "wp-cli.phar user list --field=user_login --search=${WP_USER} --path=$WP_PATH")
-if [ ! "$WP_USER" == "$USER_EXISTS" ]; then
-	echo "Creating second user..."
-	su -s /bin/bash www-data -c "wp-cli.phar --path=${WP_PATH} user create ${WP_USER} ${WP_USER_EMAIL} --role=subscriber --user_pass=${WP_USER_PASSWORD}"
-else
-	echo "Second user already created."
 fi
 exec php-fpm7.4 -F -R
